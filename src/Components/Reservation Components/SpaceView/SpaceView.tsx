52325b5894c24e4b5ee5e1ab0f6bd5e85ae34066
Daniel Rojas <danni3231@hotmail.com>
2022-03-03 17:44:14 -0500

    updated SpaceView component

--- src/Components/Reservation Components/SpaceView/SpaceView.tsx
+++ src/Components/Reservation Components/SpaceView/SpaceView.tsx
@@ -1,11 +1,35 @@
 import * as React from "react";
+import Btn from "../../Buttons/Btn";
+
+import "./SpaceView.css";
 
 interface SpaceViewProps {}
 
 const SpaceView: React.FC<SpaceViewProps> = ({}) => {
+  const onSend = () => {};
+
   return (
-    <article>
-      <img src="" alt="" />
+    <article className="spaceView">
+      <div className="spaceView__header">
+        <img
+          className="spaceView__header__img"
+          src={`${process.env.PUBLIC_URL}/Img/pool.png`}
+          alt=""
+        />
+        <div className="spaceView__header__content">
+          <p>piscina</p>
+        </div>
+      </div>
+      <form className="spaceView__form" action="">
+        <h1>Ocupacion:</h1>
+        <p>horario:</p>
+        <p>luneas a domingo</p>
+        <h1>Fecha de reserva</h1>
+        <input type="date" name="" id="" />
+        <h1>Horarios</h1>
+
+        <Btn text="Confirmar" variant="" action={onSend}></Btn>
+      </form>
     </article>
   );
 };
