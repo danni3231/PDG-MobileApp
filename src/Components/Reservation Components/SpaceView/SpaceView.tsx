e1f7b534a5e8782e128b16ef4312ddf8242b10b1
Daniel Rojas <danni3231@hotmail.com>
2022-03-07 14:15:21 -0500

    added hour count function

--- src/Components/Reservation Components/SpaceView/SpaceView.tsx
+++ src/Components/Reservation Components/SpaceView/SpaceView.tsx
@@ -19,12 +19,28 @@ const SpaceView: React.FC<SpaceViewProps> = ({}) => {
     days: { end: "", start: "" },
     schedule: { end: "", start: "" },
   });
+
   const [date, setDate] = React.useState("");
   const [schedule, setSchedule] = React.useState("");
   const [submit, setSubmit] = React.useState(false);
 
   const getSpace = async () => {
     const snapshot = await getSpaceData(id!);
+
+    let t1 = new Date(),
+      t2 = new Date();
+
+    t1.setHours(~~snapshot.data()!.schedule.end, 0, 0);
+    t2.setHours(~~snapshot.data()!.schedule.start, 0, 0);
+
+    t1.setHours(
+      t1.getHours() - t2.getHours(),
+      t1.getMinutes() - t2.getMinutes(),
+      t1.getSeconds() - t2.getSeconds()
+    );
+
+    console.log(`diferencia ${t1.getHours()} horas`);
+
     const space: space = {
       name: snapshot.data()!.name,
       img: snapshot.data()!.name,
@@ -33,6 +49,7 @@ const SpaceView: React.FC<SpaceViewProps> = ({}) => {
       schedule: snapshot.data()!.schedule,
       id: id!,
     };
+
     setSpace(space);
   };
 
@@ -51,7 +68,7 @@ const SpaceView: React.FC<SpaceViewProps> = ({}) => {
     if (date != "" && schedule != "") {
       setSubmit(true);
     }
-  });
+  }, []);
 
   return (
     <article className="spaceView">
@@ -82,7 +99,10 @@ const SpaceView: React.FC<SpaceViewProps> = ({}) => {
           <h1>Horarios</h1>
 
           <section className="spaceView__schedule">
-            <div className="spaceView__schedule__option"> 13:00 - 65 am</div>
+            {}
+            <div className="spaceView__schedule__option spaceView__schedule__option--selected">
+              13:00 - 65 am
+            </div>
             <div className="spaceView__schedule__option"> 13:00 - 65 am</div>
             <div className="spaceView__schedule__option"> 13:00 - 65 am</div>
             <div className="spaceView__schedule__option"> 13:00 - 65 am</div>
